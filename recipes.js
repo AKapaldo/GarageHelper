c77cf9f67e989a9c4bf1919a95c7eb73385a6c97
Andrew Kapaldo <53104260+AKapaldo@users.noreply.github.com>
2020-11-10 21:59:58 -0500

    Create recipes.js

---
+++ recipes.js
@@ -0,0 +1,30 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent'] */
+module.exports = {
+  'RECIPE_EN_US': {
+    '2004 jeep grand cherokee': 'Use a 13 millimeter socket on the oil pan. Add 6 Quarts of 5W30 oil. Use Mopar filter M0-090.',
+    '2003 jeep grand cherokee': 'Use a 13 millimeter socket on the oil pan. Add 6 Quarts of 5W30 oil. Use Mopar filter M0-090.',
+    '2002 jeep grand cherokee': 'Use a 13 millimeter socket on the oil pan. Add 6 Quarts of 5W30 oil. Use Mopar filter M0-090.',
+    '2001 jeep grand cherokee': 'Use a 13 millimeter socket on the oil pan. Add 6 Quarts of 5W30 oil. Use Mopar filter M0-090.',
+    '2000 jeep grand cherokee': 'Use a 13 millimeter socket on the oil pan. Add 6 Quarts of 5W30 oil. Use Mopar filter M0-090.',
+    '1999 jeep grand cherokee': 'Use a 13 millimeter socket on the oil pan. Add 6 Quarts of 5W30 oil. Use Mopar filter M0-090.',
+    'jeep jl': 'Use a 13 millimeter socket on the oil pan and a 24 millimeter socket on the oil filter housing. Add 5 Quarts of 0W20 oil. Use Mopar filter 68191349A A.',
+    '2019 jeep wrangler': 'Use a 13 millimeter socket on the oil pan and a 24 millimeter socket on the oil filter housing. Add 5 Quarts of 0W20 oil. Use Mopar filter 68191349A A.',
+    '2018 jeep wrangler jl': 'Use a 13 millimeter socket on the oil pan and a 24 millimeter socket on the oil filter housing. Add 5 Quarts of 0W20 oil. Use Mopar filter 68191349A A.',
+    '2018 jeep wrangler jk': 'Use a 13 millimeter socket on the oil pan and a 24 millimeter socket on the oil filter housing. Add 6 Quarts of 5W20 oil. Use Mopar filter 68191349A A.',
+    '2017 jeep wrangler': 'Use a 13 millimeter socket on the oil pan and a 24 millimeter socket on the oil filter housing. Add 6 Quarts of 5W20 oil. Use Mopar filter 68191349A A.',
+    '2016 jeep wrangler': 'Use a 13 millimeter socket on the oil pan and a 24 millimeter socket on the oil filter housing. Add 6 Quarts of 5W20 oil. Use Mopar filter 68191349A A.',
+    '2015 jeep wrangler': 'Use a 13 millimeter socket on the oil pan and a 24 millimeter socket on the oil filter housing. Add 6 Quarts of 5W20 oil. Use Mopar filter 68191349A A.',
+    '2014 Jeep wrangler': 'Use a 13 millimeter socket on the oil pan and a 24 millimeter socket on the oil filter housing. Add 6 Quarts of 5W20 oil. Use Mopar filter 68191349A A.',
+    '2013 jeep wrangler': 'Use a 13 millimeter socket on the oil pan and a 24 millimeter socket on the oil filter housing. Add 6 Quarts of 5W30 oil. Use Mopar filter 68079744A D.',
+    '2012 jeep wrangler': 'Use a 13 millimeter socket on the oil pan and a 24 millimeter socket on the oil filter housing. Add 6 Quarts of 5W30 oil. Use Mopar filter 68079744A D.',
+    '2005 chevrolet silverado 1500': 'Use A.C. Delco filter P.F. 46 with 6 Quarts of 5W30 oil.',
+    '2015 dodge dart': 'Add 5 quarts of 0w20 synthetic oil.',
+    'Joshs dodge dart': 'First drain the oil, then add 5 quarts of 0w20 synthetic oil.',
+    '2017 jeep renegade': 'Use a 13 millimeter socket on the oil pan. For the 2.4 liter engine Add 5 and a half quarts of 0w20 oil. Use Mopar filter 4892339A A. For the 1.4 liter engine Add 4 quarts of 5W40 Synthetic oil. Use Mopar filter 68102241A A.',
+    '2017 jeep renegade 2.4 liter': 'Use a 13 millimeter socket on the oil pan. Add 5 and a half quarts of 0w20 oil. Use Mopar filter 4892339A A.',
+    '2017 jeep renegade 1.4 liter': 'Use a 13 millimeter socket on the oil pan. Add 4 quarts of 5W40 Synthetic oil. Use Mopar filter 68102241A A.',
+    '2013 jeep grand cherokee': 'Use a 13 millimeter socket on the oil pan and a 24 millimeter socket on the oil filter housing. Add 6 quarts of 5w20 oil.',
+  },
+};
